76f012c574001de7540813c08765a6799a84a0e0
Scott Doggett <sdoggett@lcs.on.ca>
2020-05-07 16:22:38 -0400

    connections made and slider reads value

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var heightLabel: UILabel!
+    
+    @IBOutlet var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func heightValueChanged(_ sender: UISlider) {
+        print(String(format: "%.2f", sender.value))
+    }
+    
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print(Int(sender.value))
+    }
+    
+    
 
 }
 
